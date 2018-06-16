80e1c58f8d136485138e06871b2880a2fd2d95b4
dakpek <dorukakpek@gmail.com>
2018-06-16 14:18:50 +0200

    feat(db.js, controllers, models): readding db, controllers, models

---
+++ db.js
@@ -0,0 +1,41 @@
+const db         = require('mongoose');
+const Message    = require('./models/model-message')
+
+// SENDER ENDPOINTS ================
+
+const postHash = (message) => {
+  Message.create(message)
+     .then(res => console.log("successfully added item"))
+    .catch(err => console.error(err))
+}
+
+// RECEIVER ENDPOINTS ==============
+
+function DateError(message) {
+  this.message = message;
+}
+
+DateError.prototype = new Error();
+
+const getHash = async (hash) => {
+     let data = await Message.findOne({"hash": hash})
+     let dateNow = new Date()
+     if (data.dateExpire.getTime() >= dateNow.getTime()) {
+       return data
+     } else {
+       throw new DateError('This message has expired')
+     }
+}
+
+// DATABASE CONNECTION =============
+
+db.connect('mongodb://localhost/FileNation')
+    .then(() => console.log('Connection Successful!'))
+  .catch(err => console.error(err));
+
+
+// EXPORTS =======================
+
+exports.db       = db
+exports.postHash = postHash
+exports.getHash  = getHash
